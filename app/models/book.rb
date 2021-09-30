36556c14103ec66f9d512f4ed8b11d364593a5d1
pranavPk1 <pranav1999@gmail.com>
2021-09-30 18:30:26 +0000

    book collection mostly works

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
+  # validates that each attribute isn't empty
     validates :title, presence: true
+    validates :Author, presence: true
+    #validates :Price, numericality: true,
+    # validates that inputted price is a positive, real number
+    validates_numericality_of :Price, :greater_than_or_equal => 0.0, presence: true
+    validates :date, presence: true
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,15 +3,39 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', Author: 'J.K. Rowling', Price: 50.0, date: '2021-08-06')
   end
 
+# attributes are validated
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
+# If it the bookTitle is empty, error is displayed
   it 'is not valid without a name' do
     subject.title = nil
     expect(subject).not_to be_valid
   end
+
+#  it 'is valid with valid attributes' do
+#    expect(subject).to be_valid
+#  end
+
+# If author is empty, error is displayed
+  it 'is not valid without a name' do
+    subject.Author = nil
+    expect(subject).not_to be_valid
+  end
+  
+# If Price is empty, error is displayed
+  it 'is not valid without a numeric' do
+    subject.Price = nil
+    expect(subject).not_to be_valid
+  end
+# If date is empty, error is displayed
+  it 'is not valid without a date' do
+    subject.date = nil
+    expect(subject).not_to be_valid
+  end
+
 end
